1bc66bbaa3bbfdb4f81cb3ffc6c4ff209ceaf63a
Han Wang <han-wang@outlook.com>
2015-07-09 22:26:58 +0800

    Controller unfinished

--- single_cycle/Controller.v
+++ single_cycle/Controller.v
@@ -3,20 +3,20 @@
 //2013011076 Wang Han
 //Controller, incomplete
 
-module Controller ( .OpCode,
-					.Funct,
-					.IRQ,
-					.PCSrc,
-					.RegWrite,
-					.RegDst, 
-					.MemRead,
-					.MemWrite,
-					.MemtoReg,
-					.ALUSrc1,
-					.ALUSrc2,
-					.ExtOp,
-					.LuOp,
-					.ALUOp);
+module Controller ( OpCode,
+					Funct,
+					IRQ,
+					PCSrc,
+					RegWrite,
+					RegDst, 
+					MemRead,
+					MemWrite,
+					MemToReg,
+					ALUSrc1,
+					ALUSrc2,
+					ExtOp,
+					LuOp,
+					ALUOp);
 	input  [5:0] OpCode;
 	input  [5:0] Funct;
 	input 	IRQ;
@@ -25,11 +25,38 @@ module Controller ( .OpCode,
 	output [1:0] RegDst;
 	output MemRead;
 	output MemWrite;
-	output [1:0] MemtoReg;
+	output [1:0] MemToReg;
 	output ALUSrc1;
 	output ALUSrc2;
 	output ExtOp;
 	output LuOp;
 	output [1:0] ALUOp;
+	
+	assign PCSrc =
+		(OpCode == 6'h02 || OpCode == 6'h03) ? 2'b10 :
+		(OpCode == 6'h00 && (Funct == 6'h08 || Funct == 6'h09)) ? 2'b11 :
+		(OpCode >= 6'h01 && OpCode <= 6'h07) ? 2'b01 :
+		2'b00;
+		
+	assign RegWrite = (IRQ) ? 1'b1 :
+			(OpCode == 6'h2b || (OpCode >= 6'h04 && OpCode <= 6'h07) || OpCode == 6'h01 || OpCode == 6'h02 || (OpCode == 6'h00 && Funct == 6'h08)) ? 1'b0 : 1'b1;
+			
+	assign RegDst = (IRQ) ? 2'b11 :
+		(OpCode >= 6'h08) ? 2'b00 :
+		(OpCode == 6'h03) ? 2'b10 :
+		2'b01;
+	
+	assign MemRead = (IRQ) ? 1'b0 :
+					(OpCode == 6'h23) ? 1'b1 : 1'b0;
+	
+	assign MemWrite = (IRQ) ? 1'b0 :
+					(OpCode == 6'h2b) ? 1'b1 : 1'b0;
+	
+	assign MemToReg = (IRQ) ? 2'b10 :
+		(OpCode == 6'h23) ? 2'b01 :
+		(OpCode == 6'h03 || (OpCode == 6'h00 && Funct == 6'h09)) ? 2'b10 :
+		2'b00;
+		
+	
 	
 endmodule
