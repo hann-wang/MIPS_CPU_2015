5737f777525fadf588f89840f72f68250854f6ce
HackerWang <han-wang@outlook.com>
2015-07-15 10:56:45 +0800

    remove unused parameters

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -10,28 +10,9 @@ module ALU (input	[31:0]	A,
 			output	[31:0]	ALUOut);
 
 	parameter ALU_ARITH = 2'b00;
-	parameter ALUFUNC_ADD = 6'b000000;
-	parameter ALUFUNC_SUB = 6'b000001;
-
 	parameter ALU_LOGIC = 2'b01;
-	parameter ALUFUNC_AND = 6'b011000;
-	parameter ALUFUNC_OR  = 6'b011110;
-	parameter ALUFUNC_XOR = 6'b010110;
-	parameter ALUFUNC_NOR = 6'b010001;
-	parameter ALUFUNC_A   = 6'b011010;
-
 	parameter ALU_SHIFT = 2'b10;
-	parameter ALUFUNC_SLL = 6'b100000;
-	parameter ALUFUNC_SRL = 6'b100001;
-	parameter ALUFUNC_SRA = 6'b100011;
-
 	parameter ALU_CMP = 2'b11;
-	parameter ALUFUNC_EQ  = 6'b110011;
-	parameter ALUFUNC_NEQ = 6'b110001;
-	parameter ALUFUNC_LT  = 6'b110101;
-	parameter ALUFUNC_LEZ = 6'b111101;
-	parameter ALUFUNC_GEZ = 6'b111001;
-	parameter ALUFUNC_GTZ = 6'b111111;
 
 	wire	[31:0]	ArithOut;
 	wire			Zero;
