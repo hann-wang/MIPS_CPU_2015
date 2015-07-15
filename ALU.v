30fa1966feefdfe8b832b6e6f18d9d190b6c9cf3
CCCharlesChan <chen-zj13@mails.tsinghua.edu.cn>
2015-07-15 10:08:21 +0800

    update ALU.v

--- ALU.v
+++ ALU.v
@@ -8,12 +8,16 @@ module ALU(
 	input [5:0] fun;
 	input sign;
 	output [31:0] Z;
-	wire [31:0]z1,z2,z3,z4;
+	wire [31:0]z1,z2,z3,z4,b;
+	wire z,v,n;
 	
-	Fun_00 f1(A,B,fun[0],sign,z1);
+	assign b=
+	(fun[5:3]==3'b111)?32'b0:B;
+	
+	Fun_00 f1(A,b,fun[0],sign,z1,z,v,n);
 	Fun_01 f2(A,B,fun[3:0],z2);
 	Fun_10 f3(A,B,fun[1:0],z3);
-	Fun_11 f4(A,B,fun[3:1],sign,z4);
+	Fun_11 f4(fun[3:1],z,v,n,z4);
 	
 	assign	Z=
 	(fun[5:4]==2'b00)?z1:
